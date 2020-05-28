8887f78a75dd4617f01908bae43006ecfbbcb70b
prgofficial <65294906+prgofficial@users.noreply.github.com>
2020-05-28 18:57:08 +0530

    Create config.py

---
+++ skylee/config.py
@@ -0,0 +1,13 @@
+from skylee.sample_config import Config
+
+class Config(object):
+    API_KEY = "1162349467:AAFEUWPUHTfZjVv_FXuJIGWayZ21UBrqZhg"
+    OWNER_ID = "680815375"
+    OWNER_USERNAME = "prgofficial"
+    TELETHON_HASH = "7456c8b2dd22c11947f1999e06a53c0e"
+    TELETHON_ID = "1201633"
+    SQLALCHEMY_DATABASE_URI = 'postgres://kwwohgkampxell:bc22f47d84b164bd58729b2ca693e093ace2b69c248739b8a0ba9624f4c23a3d@ec2-34-230-149-169.compute-1.amazonaws.com:5432/d20tq0ov7uqhg3'
+    LOAD = []
+    NO_LOAD = []
+    PORT = 8443
+    URL = 'https://moantiflood.herokuapp.com/'
